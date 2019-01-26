9bac436f73c8bf87f32186859a0c5dfeec0360da
Michael Kim <m.kim4247@gmail.com>
2019-01-26 16:34:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
+  let min = array[0]
+  let index = 0
 
+  for(let i = 1; i < array.length; i++){
+    if(array[i] < min){
+      min = array[i]
+      index = i
+    }
+  }
+  array.splice(index, 1)
+  return min
 }
 
 function insertionSort(array){
+  let sorted = []
+  let i = 0
 
+  while(i < array.length){
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
